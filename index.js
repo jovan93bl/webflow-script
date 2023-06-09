a85898ed533e469139a387fe1b91703cfccec805
jovan93bl <jovantod93@gmail.com>
2023-06-09 15:13:21 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Import stylesheets
+import './style.css';
+
+
+
+/* <script>
+	if(document)  {
+    function toggleSearchContainer() {
+      if (searchContainer.style.display === 'none') {
+        searchContainer.style.display = 'block';
+      } else {
+        searchContainer.style.display = 'none';
+      }
+    }
+    document.getElementById('search-button').addEventListener('click', toggleSearchContainer);
+    const searchContainer = document.getElementsByClassName('search-container');
+  }
+</script> */
